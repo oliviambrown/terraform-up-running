f660e5304e69db1a4794dd742f5c9cfa1fe58c6c
Olivia Brown <brownoli22@gmail.com>
2023-06-27 17:01:26 -0400

    asg

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro" 
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     tags = {
       Name = "terraform-example"
@@ -13,8 +13,8 @@ resource "aws_instance" "example" {
 
     user_data = <<-EOF
                 #!/bin/bash
-                echo "Hello, World > index.html
-                nohup busybox httpd -f -p 8080 &
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
     
     user_data_replace_on_change = true
@@ -24,10 +24,23 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+  
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+  
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the webserver"
   
 }
