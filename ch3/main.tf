e69ada087be88b25e2ffa4dfc4a6c64b79cd47ae
Olivia Brown <brownoli22@gmail.com>
2023-06-28 14:46:17 -0400

    s3 backend

--- ch3/main.tf
+++ ch3/main.tf
@@ -56,3 +56,14 @@ resource "aws_dynamodb_table" "terraform_locks" {
   
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-omb"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+   
+  }
+}
