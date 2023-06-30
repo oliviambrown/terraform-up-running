4ee89a16752f64674f73e09cb323b8c6c0c933da
Olivia Brown <brownoli22@gmail.com>
2023-06-30 15:45:47 -0400

    update for tags

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -40,3 +40,10 @@ variable "max_size" {
     type = number
   
 }
+
+variable "custom_tags" {
+    description = "Custom tags to set on the Instances in the ASG"
+    type = map(string)
+    default = {}
+  
+}
